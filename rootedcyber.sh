f0bda9adafdfadbf46f065f502ebe63dbba2b696
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-08-16 18:27:25 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -2,5 +2,6 @@ git clone https://github.com/DARK-COBRA/DARKCOBRA
 cd DARK*
 pip install --upgrade pip
 pip3 install -r req*
+pip3 install validators
 pip3 install telethon==1.24.0
 python3 -m userbot
